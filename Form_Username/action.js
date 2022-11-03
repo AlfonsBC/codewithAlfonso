84715cd21a41bb5e922b961564bef216a2618a12
Alfonso Barajas <63176184+AlfonsBC@users.noreply.github.com>
2022-11-03 05:11:45 -0500

    Adding changes to files

--- Form_Username/action.js
+++ Form_Username/action.js
@@ -1,15 +1,16 @@
-//when i click on my button
-document.getElementById('fillForm').addEventListener('click', function(){
-    //grab the text filed value from my tool
-    var intValue = document.getElementById('apivalue').value;
-
-    chrome.tabs.executeScript({
-        //send the value to be used by our script
-        code: `var name = ${intValue};`
-    }, function() {
-        //run the script in the file injector.js
-        chrome.tabs.executeScript({
-            file: 'injector.js'
-        });
-    });
-  });
+const scriptUrl = "https://script.google.com/macros/s/AKfycbxiY8elpU_iSNvVsJRKrCao4LLLNwE837Yo0Xftmkk4HCqcdclCicJ_mciAGX6RMB_D/exec";
+let form = document.querySelector("form");
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            document.querySelector("#sub").value = "Submiting..";
+            let data = new FormData(form);
+            fetch(scriptUrl, {
+                    method: "POST",
+                    body: data
+                })
+                .then(res => res.text())
+                .then(data => {
+                    document.querySelector("#msg").innerHTML = data;
+                    document.querySelector("#sub").value = "Submit"
+                });
+        })
